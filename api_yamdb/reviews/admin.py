403d3042c1ab88647ce735def6803f6d4fb3fc38
Максим Бризганов <tapoloker@gmail.com>
2022-12-15 17:46:18 +0300

    Configured the admin panel for Title, Genre, Category

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,2 +1,34 @@
 from django.contrib import admin
 from .models import Category, Comment, Genre, Review, Title
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'year',
+        'descriptions',
+        'genre',
+        'category',
+    )
+    list_editable = ('genre', 'category',)
+    search_fields = ('name',)
+    list_filter = ('year',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
