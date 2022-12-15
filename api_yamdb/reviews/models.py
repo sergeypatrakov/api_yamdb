6824a412977073e05acfd856d323b22f95cf7a58
Максим Бризганов <tapoloker@gmail.com>
2022-12-15 17:38:46 +0300

    Add model Category

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,25 @@
 from django.db import models
+
 from users.models import User
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=256,
+    )
+    slug = models.SlugField(
+        verbose_name='Строка идентификатор категории',
+        unique=True,
+        max_length=50,
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
 
 
 class Genre(models.Model):
