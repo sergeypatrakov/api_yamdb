b705b079bd59c5e24b1a21e28bb85ded4be1e9ef
Максим Бризганов <tapoloker@gmail.com>
2022-12-15 17:41:10 +0300

    Create year's validator

---
+++ api_yamdb/reviews/validators.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from django.core.exceptions import ValidationError
+
+
+def validate_correct_year(year):
+    """Проверяет, что год создания произведения <= текущему"""
+    current_year = datetime.now().year
+    if year > current_year:
+        raise ValidationError(
+            'Год написания произведения не может быть больше текущего',
+            params={'value': year},
+        )
