39bdb4f904dc3c49ef6c8a6b093056ee4adf448c
nm0i <nm0i@me0w.net>
2022-12-15 23:28:26 +0300

    Admin panel for user model

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    search_fields = ('email', 'first_name', 'last_name')
+    list_display = ('username', 'email', 'role', 'date_joined')
+    list_editable = ('role',)
