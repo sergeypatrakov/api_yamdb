69059f96b3acc8f002feb01be602934f0569f859
nm0i <nm0i@me0w.net>
2022-12-16 16:43:22 +0300

    Refactoring user role definitions.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,13 +1,10 @@
 from rest_framework import permissions
 
-from users.models import USER_ROLES
-
 
 class AdminOrReadOnly(permissions.BasePermission):
-
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-                and request.user.role == USER_ROLES[2][0]
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+            and request.user.is_admin
         )
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,21 +3,36 @@ from typing import Tuple
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-USER_ROLES: Tuple[Tuple[str, ...], ...] = (
-    ("user", "Аутентифицированный пользователь"),
-    ("moderator", "Модератор"),
-    ("admin", "Администратор"),
-)
-
 
 class User(AbstractUser):
+    USER_ROLE: str = "user"
+    MODERATOR_ROLE: str = "moderator"
+    ADMIN_ROLE: str = "admin"
+    ROLE_CHOICES: Tuple[Tuple[str, ...], ...] = (
+        (USER_ROLE, "Аутентифицированный пользователь"),
+        (MODERATOR_ROLE, "Модератор"),
+        (ADMIN_ROLE, "Администратор"),
+    )
+
     bio = models.TextField(
-        verbose_name='Биография',
+        verbose_name="Биография",
         blank=True,
     )
     role = models.CharField(
         verbose_name="Роль",
         max_length=32,
-        choices=USER_ROLES,
-        default='user',
+        choices=ROLE_CHOICES,
+        default="user",
     )
+
+    @property
+    def is_user(self):
+        return self.role == User.USER_ROLE
+
+    @property
+    def is_moderator(self):
+        return self.role == User.MODERATOR_ROLE
+
+    @property
+    def is_admin(self):
+        return self.role == User.ADMIN_ROLE
