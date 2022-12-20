7e00612b5411fa7692877305ee88d8caa9421fc4
nm0i <nm0i@me0w.net>
2022-12-20 22:25:18 +0300

    Refactoring db_import command

---
+++ api_yamdb/import.sql
@@ -0,0 +1,7 @@
+.import --csv static/data/category.csv reviews_category
+.import --csv static/data/comments.csv reviews_comment
+.import --csv static/data/genre.csv reviews_genre
+.import --csv static/data/review.csv reviews_review
+.import --csv static/data/titles.csv reviews_title
+.import --csv static/data/genre_title.csv reviews_genretitle
+.import --csv static/data/users.csv users_user
