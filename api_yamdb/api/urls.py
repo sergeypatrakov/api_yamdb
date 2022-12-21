9a60524bc135f372c93d8040bd1b2c32cd754836
Максим Бризганов <tapoloker@gmail.com>
2022-12-21 21:28:05 +0300

    Add endpoints for Title

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import CategoryViewSet, GenreViewSet
+from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 app_name = 'api'
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'genres', GenreViewSet)
+router.register(r'titles', TitleViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
