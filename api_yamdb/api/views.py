3b8114b771669dad4407c2c24f8c36d05a7216ec
Максим Бризганов <tapoloker@gmail.com>
2022-12-21 21:29:10 +0300

    Add view for Title

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,9 +1,11 @@
-from rest_framework import mixins
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import mixins, permissions
 from rest_framework import viewsets, filters
 
-from reviews.models import Category, Genre
+from reviews.filters import GenreFilter
+from reviews.models import Category, Genre, Title
 from .permissions import AdminOrReadOnly
-from .serializers import CategorySerializer, GenreSerializer
+from .serializers import CategorySerializer, GenreSerializer, GetTitleSerializer, PostPutPatchDeleteTitleSerializer
 
 
 class CreateListDeleteViewSet(
@@ -31,3 +33,15 @@ class GenreViewSet(CreateListDeleteViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    permission_classes = (AdminOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = GenreFilter
+
+    def get_serializer_class(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return GetTitleSerializer
+        return PostPutPatchDeleteTitleSerializer
