9ee7f2385b9e981a4e404a96f5d666c80b496d56
nm0i <nm0i@me0w.net>
2022-12-25 14:43:04 +0300

    More isort "fixes"

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,8 +1,8 @@
-from api_yamdb import settings
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
+from api_yamdb import settings
 from reviews.models import Category, Comment, Genre, Review, Title, TitleGenre
 from users.models import User
 
